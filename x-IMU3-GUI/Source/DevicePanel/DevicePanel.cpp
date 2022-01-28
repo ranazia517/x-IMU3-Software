25a48dfe4349dd221a0b751fc2892bbed4d74f3d
Jelle Bakker <jellebakker@jb-audio.com>
2022-01-27 17:36:48 -0800

    Simplify changing layout behaviour

--- x-IMU3-GUI/Source/DevicePanel/DevicePanel.cpp
+++ x-IMU3-GUI/Source/DevicePanel/DevicePanel.cpp
@@ -36,6 +36,7 @@ DevicePanel::DevicePanel(const juce::ValueTree& windowLayout_,
 DevicePanel::~DevicePanel()
 {
     connection->removeCallback(decodeErrorCallbackID);
+    connection->close();
 }
 
 void DevicePanel::resized()
